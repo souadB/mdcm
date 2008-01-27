ac6c1e30cc0e7ae60030c9d3f8ef6ad516255c7d
colby.dillion <colby.dillion@747b31cf-ab44-0410-8078-1f7da440c36a>
2008-01-27 05:33:16 +0000

    DicomDump

---
+++ trunk/DicomDump/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+using Dicom.Data;
+using Dicom.Forms;
+
+namespace DicomDump {
+	static class Program {
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args) {
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			//Dicom.Codec.ExtDicomCodec.Initialize();
+
+			if (File.Exists("dicom.dic"))
+				DcmDictionary.ImportDictionary("dicom.dic");
+			else
+				DcmDictionary.LoadInternalDictionary();
+
+			if (File.Exists("private.dic"))
+				DcmDictionary.ImportDictionary("private.dic");
+
+			DicomDumpForm form = new DicomDumpForm();
+			foreach (string file in args) {
+				form.AddFile(file);
+			}
+
+			Application.Run(form);
+		}
+	}
+}
